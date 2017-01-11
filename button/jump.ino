c73f49b69d56a80a2ca8e13da69f028b436c0b23
TaiChi <jesse.guanyu@gmail.com>
2017-01-11 16:40:22 +0800

    Create jump.ino

---
+++ button/jump.ino
@@ -0,0 +1,36 @@
+byte SW = 2;
+byte Red = 12;
+byte Click = 0;
+boolean b1, b2;
+boolean lastStatus = LOW;
+boolean lightStatus = LOW;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode( SW, INPUT );
+  pinMode( Red, OUTPUT );
+  digitalWrite( Red, lightStatus );
+  lastStatus = digitalRead( SW );
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  b1 = digitalRead( SW );
+
+
+  if( Click == 2 )
+  {
+    Click = 0;
+    lightStatus = !lightStatus;
+    digitalWrite( Red, lightStatus );
+  }
+  else
+  {
+    if( b1 != lastStatus )
+    {
+      delay( 20 );
+      lastStatus = b1;
+      Click += 1;
+    }
+  }
+}
